66462d71d1f4bcc86501b06de1971b5898d9c9ce
Artemeva Marina <rinko-69@yandex.ru>
2022-12-19 13:09:24 +0300

    Разделила БД

--- backend/recipes/management/commands/load_ingrds.py
+++ backend/recipes/management/commands/load_ingrds.py
@@ -3,12 +3,12 @@ import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient
 
 DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
 
 
-class IngrCommand(BaseCommand):
+class Command(BaseCommand):
     help = 'loading ingredients from data in json or csv'
 
     def add_arguments(self, parser):
@@ -28,24 +28,3 @@ class IngrCommand(BaseCommand):
                     )
         except FileNotFoundError:
             raise CommandError('Добавьте файл ingredients в директорию data')
-
-
-class TagCommand(BaseCommand):
-
-    def add_arguments(self, parser):
-        parser.add_argument('filename', default='tags.csv', nargs='?',
-                            type=str)
-
-    def handle(self, *args, **options):
-        try:
-            with open(os.path.join(DATA_ROOT, options['filename']), 'r',
-                      encoding='utf-8') as f:
-                datareader = csv.reader(f)
-                for row in datareader:
-                    Tag.objects.get_or_create(
-                        name=row[0],
-                        color=row[1],
-                        slug=row[2]
-                    )
-        except FileNotFoundError:
-            raise CommandError('Добавьте файл tags в директорию data')
