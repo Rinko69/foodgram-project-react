26ef7a7aa7a9b33e645baa620f36be067a392c8d
Artemeva Marina <rinko-69@yandex.ru>
2022-12-23 15:02:24 +0300

    Еще правки

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -45,10 +45,12 @@ class CustomUserSerializer(UserSerializer):
             'is_subscribed')
 
     def get_is_subscribed(self, obj):
-        user = self.context.get('request').user
-        if user.is_anonymous:
+        request = self.context.get('request')
+        if not request or request.user.is_anonymous:
             return False
-        return Follow.objects.filter(user=user, author=obj.id).exists()
+        return Follow.objects.filter(user=self.context['request'].user,
+                                     author=obj).exists()
+
 
 
 class FollowRecipeSerializer(serializers.ModelSerializer):
