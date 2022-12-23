4b3937ad55f9864a376cdeb61e695e3f7d000191
Artemeva Marina <rinko-69@yandex.ru>
2022-12-23 15:46:56 +0300

    Поправила urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,22 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CustomUserViewSet, FollowListView, FollowViewSet
+from .views import SubscriptionListView, follow_author
 
 router = DefaultRouter()
-router.register('users', CustomUserViewSet, basename='users')
+router.register(
+    r'users/subscriptions',
+    SubscriptionListView,
+    basename='subscriptions',
+)
 
 urlpatterns = [
-    path(
-        'users/subscriptions/',
-        FollowListView.as_view(),
-        name='subscriptions'
-    ),
-    path(
-        'users/<int:user_id>/subscribe/',
-        FollowViewSet.as_view(),
-        name='subscribe'
-    ),
+     path('users/<int:pk>/subscribe/',
+         follow_author,
+         name='follow-author'),
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
